5a1605d76e2f13d07098362c66e29225677a8a11
Andrea <ac2343@cornell.edu>
2022-11-21 17:04:53 -0500

    Updated merge conflicts and small changes to main.ml

---
+++ _build/default/src/state.ml
@@ -0,0 +1,90 @@
+open Account
+
+type t = {
+  mutable current_account : Account.t option;
+  mutable accounts : Account.t array;
+      (* mutable transactions : Account.transaction list; *)
+}
+
+exception InvalidUsername of string
+exception IncorrectPassword
+
+let init_state =
+  { current_account = None; accounts = [||] (*transactions = []*) }
+
+let current_account st = st.current_account
+let accounts st = st.accounts
+
+(* let transactions st = st.transactions *)
+let add_account st acc = st.accounts <- Array.append [| acc |] st.accounts
+
+let delete_account st id =
+  st.accounts.(id) <- Account.deactivate st.accounts.(id)
+
+let check_username st un =
+  let username_list =
+    Array.map (fun acc -> Account.username acc) (accounts st)
+  in
+  if Array.mem un username_list then raise (InvalidUsername un)
+
+let make_payment st paying_acc_id paid_acc_id p =
+  st.accounts.(paying_acc_id) <- withdraw st.accounts.(paying_acc_id) p;
+  st.accounts.(paid_acc_id) <- deposit st.accounts.(paid_acc_id) p
+
+let rec array_find a p n =
+  if n > Array.length a - 1 then failwith "Not found"
+  else if p a.(n) then n
+  else array_find a p (n + 1)
+
+let find_un st un = array_find st.accounts (fun a -> username a = un) 0
+
+let make_deposit st un p =
+  st.accounts.(find_un st un) <- deposit st.accounts.(find_un st un) p
+
+(* let execute_transaction st t = match t with | Pay { payer; payee; amount } ->
+   (try check_username st payer with InvalidUsername payer -> failwith "Invalid
+   payer username"); (try check_username st payee with InvalidUsername payee ->
+   failwith "Invalid payee username"); make_payment st (find_un st payer)
+   (find_un st payee) amount | Request { payer; payee; amount; accepted } ->
+   (try check_username st payer with InvalidUsername payer -> failwith "Invalid
+   payer username"); (try check_username st payee with InvalidUsername payee ->
+   failwith "Invalid payee username"); if accepted then make_payment st (find_un
+   st payer) (find_un st payee) amount else () *)
+
+let logout st = st.current_account <- None
+let acc st un = (accounts st).(find_un st un)
+let login_state st un pass = Account.check_password pass (acc st un)
+
+let login_system st un pass =
+  if login_state st un pass then st.current_account <- Some (acc st un)
+
+(********************************************************************** Writing
+  to json
+  **********************************************************************)
+let to_json st : Yojson.Basic.t =
+  `Assoc
+    [
+      ( "current account",
+        `String
+          (match current_account st with
+          | None -> "none"
+          | Some acc -> username acc) );
+      ("accounts", `List Array.(to_list (map Account.to_json (accounts st))))
+      (* ("transactions", ) *);
+    ]
+
+let test = true
+
+let to_file st =
+  let file = if test then "data/test_data.json" else "data/data.json" in
+  let oc = open_out file in
+  Yojson.Basic.to_channel oc (to_json st);
+  close_out oc
+
+let add_notif_inbox st payer notif =
+  Account.add_notification st.accounts.(find_un st payer) notif
+
+(** notification inbox : notification inbox exist in account.ml t type.
+    Whenever, the account makes a request, it returns a notification and this
+    notification will be added to the payer's notification_inbox. For now, the
+    user can only clear all of the requests in the inbox *)
---
+++ _build/default/src/state.mli
@@ -0,0 +1,68 @@
+(** Representation of dynamic Venmo state.
+
+    This module represents the state of a VenmOCaml system since its start,
+    including the accounts that have been created, a history of transactions,
+    and functions that cause the state to change. *)
+
+(* type transaction *)
+(** The abstract type of values representing a VenmOCaml transaction. *)
+
+type t
+(** The abstract type of values representing the VenmOCaml state. *)
+
+exception InvalidUsername of string
+(** Raised when the user attempts to create an account with a username that is
+    already associated with an existing account. *)
+
+exception IncorrectPassword
+(** Raised when the user enters the incorrect password for an account. *)
+
+val init_state : t
+(** [init_state] is the initial state of the Venmo system. The user is not
+    logged into any account and there have been no accounts created or
+    transactions made. *)
+
+val check_username : t -> string -> unit
+(** [check_username st un] raises [InvalidUsername un] if [un] is already
+    associated with an account in the current state, and returns [()] otherwise. *)
+
+val current_account : t -> Account.t option
+(** [current_account st] is the account that the user is currently logged into
+    in state [st]. *)
+
+val accounts : t -> Account.t array
+(** [accounts st] is a set-like array of the accounts that have been created in
+    state [st]. *)
+
+(* val transactions : t -> transaction list *)
+(** [transactions st] is a list of the transactions that have been made in state
+    [st], ordered from least recent to most recent. *)
+
+val add_account : t -> Account.t -> unit
+(** [add_account st acc] adds [acc] to the list of accounts in [st] and returns
+    [()]. *)
+
+val delete_account : t -> int -> unit
+(** [delete_account st acc] makes the account identified by [id] inactive in
+    [st] and returns [()]. *)
+
+val make_deposit : t -> string -> string -> unit
+(** [make_dposit st un p] increases the balance of the account with username
+    [un]*)
+
+val make_payment : t -> int -> int -> string -> unit
+(**[make_payment st
+  paying_acc_id paid_acc_id p] adds [p] to the balance of
+   account identified by [paid_acc_id] and removes [p] from the balance of the
+   account identified by [paying_acc_id]*)
+
+val login_system : t -> string -> string -> unit
+(** [login_system st un pass] changes current account of the state if the
+    username and password input matches one of the accounts in the state*)
+
+val logout : t -> unit
+(** [logout st] removes the current account of the state*)
+
+val to_file : t -> unit
+(** [to_file st] converts the state [st] to a JSON object and stores it in
+    "data/data.json". *)
